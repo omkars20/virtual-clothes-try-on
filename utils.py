06346ac4162fb2e52232f063b0c9b57ffaa655e1
Omkar Singh <86602101+omkars20@users.noreply.github.com>
2024-12-03 11:38:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -116,17 +116,3 @@ def serve_static_file(filename):
 
 
 
-# from twilio.rest import Client
-
-# account_sid = 'AC9f7614fe2ff1db17167eefb8ce5d77a5'
-# auth_token = '[AuthToken]'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages.create(
-#   from_='whatsapp:+14155238886',
-#   content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
-#   content_variables='{"1":"12/1","2":"3pm"}',
-#   to='whatsapp:+918976767220'
-# )
-
-# print(message.sid)
